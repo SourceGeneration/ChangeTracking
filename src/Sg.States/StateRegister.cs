7dd94dd754aa70938393eb7261e6e97d8c3013a7
肖昕 <xiao.xin@outlook.com>
2023-12-17 22:36:31 +0800

    Initial commit

--- src/Sg.States/StateRegister.cs
+++ src/Sg.States/StateRegister.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Sg.ChangeTracking;
+using System.ComponentModel;
 
 namespace Sg.States;
 
+[EditorBrowsable(EditorBrowsableState.Never)]
 public static class StateRegister
 {
     private static readonly List<ServiceDescriptor> _services = [];
 
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public static void Add<TState>() where TState : class
     {
         _services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()), ServiceLifetime.Scoped));
@@ -14,6 +17,7 @@ public static class StateRegister
         _services.Add(new ServiceDescriptor(typeof(IState<TState>), p => new State<TState>((State<TState>)p.GetRequiredService<IStore<TState>>()), ServiceLifetime.Transient));
     }
 
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public static void Register(IServiceCollection services)
     {
         foreach (var service in _services)
