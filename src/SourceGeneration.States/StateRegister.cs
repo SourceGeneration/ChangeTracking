1d290fac31ba5290e55c4eb4d915d021de34d042
肖昕 <xiao.xin@outlook.com>
2024-02-17 06:46:29 +0100

    Add ServiceLifttime support

--- src/SourceGeneration.States/StateRegister.cs
+++ src/SourceGeneration.States/StateRegister.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using SourceGeneration.States;
 using System.ComponentModel;
 
 namespace SourceGeneration.States;
@@ -9,15 +8,13 @@ public static class StateRegister
 {
     private static readonly List<ServiceDescriptor> _services = [];
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static void Add<TState>() where TState : class
+    public static void Add<TState>(ServiceLifetime lifetime = ServiceLifetime.Scoped) where TState : class
     {
-        _services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()), ServiceLifetime.Scoped));
-        _services.Add(new ServiceDescriptor(typeof(IStore<TState>), p => new State<TState>(p.GetRequiredService<TState>()), ServiceLifetime.Scoped));
+        _services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()), lifetime));
+        _services.Add(new ServiceDescriptor(typeof(IStore<TState>), p => new State<TState>(p.GetRequiredService<TState>()), lifetime));
         _services.Add(new ServiceDescriptor(typeof(IState<TState>), p => new State<TState>((State<TState>)p.GetRequiredService<IStore<TState>>()), ServiceLifetime.Transient));
     }
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public static void Register(IServiceCollection services)
     {
         foreach (var service in _services)
