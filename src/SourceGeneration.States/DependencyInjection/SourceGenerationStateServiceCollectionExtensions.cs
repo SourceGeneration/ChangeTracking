d852cc1f6d84b30f86f9713175b891966cb83ed3
肖昕 <xiao.xin@outlook.com>
2024-02-17 12:43:42 +0100

    merge StateRegister

--- src/SourceGeneration.States/DependencyInjection/SourceGenerationStateServiceCollectionExtensions.cs
+++ src/SourceGeneration.States/DependencyInjection/SourceGenerationStateServiceCollectionExtensions.cs
@@ -1,23 +1,44 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SourceGeneration.States;
 
 public static class SourceGenerationStateServiceCollectionExtensions
 {
-    public static IServiceCollection AddState<TState>(this IServiceCollection services, ServiceLifetime storeLifetime = ServiceLifetime.Scoped) where TState : class
+    private static readonly List<ServiceDescriptor> _services = [];
+
+    public static IServiceCollection AddStateInjection(this IServiceCollection services)
+    {
+        foreach (var service in _services)
+        {
+            services.Add(service);
+        }
+        return services;
+    }
+
+    public static IServiceCollection AddState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(this IServiceCollection services, ServiceLifetime storeLifetime = ServiceLifetime.Scoped) where TState : class
     {
         if (storeLifetime == ServiceLifetime.Singleton)
         {
             services.AddSingleton(_ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()));
-            services.AddSingleton<IStore<TState>, State<TState>>();
+            services.AddSingleton<State<TState>, State<TState>>();
         }
         else
         {
             services.AddScoped(_ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()));
-            services.AddScoped<IStore<TState>, State<TState>>();
+            services.AddScoped<State<TState>, State<TState>>();
         }
-        services.AddTransient<IState<TState>, State<TState>>(services => new State<TState>((State<TState>)services.GetRequiredService<IStore<TState>>()));
+        services.AddTransient<IState<TState>, State<TState>>(services => new State<TState>(services.GetRequiredService<State<TState>>()));
         return services;
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static void Add<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(ServiceLifetime lifetime = ServiceLifetime.Scoped) where TState : class
+    {
+        _services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()), lifetime));
+        _services.Add(new ServiceDescriptor(typeof(State< TState>), p => new State<TState>(p.GetRequiredService<TState>()), lifetime));
+        _services.Add(new ServiceDescriptor(typeof(IState<TState>), p => new State<TState>(p.GetRequiredService<State<TState>>()), ServiceLifetime.Transient));
     }
 
 }
