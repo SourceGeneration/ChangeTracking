667bafc1e0d448a73426ccca43aa6b703e074bdc
肖昕 <xiao.xin@outlook.com>
2024-02-17 06:09:20 +0100

    rename class

---
+++ src/SourceGeneration.States/DependencyInjection/SourceGenerationStateServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SourceGeneration.States;
+
+public static class SourceGenerationStateServiceCollectionExtensions
+{
+    public static IServiceCollection AddState<TState>(this IServiceCollection services, ServiceLifetime storeLifetime = ServiceLifetime.Scoped) where TState : class
+    {
+        if (storeLifetime == ServiceLifetime.Singleton)
+        {
+            services.AddSingleton(_ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()));
+            services.AddSingleton<IStore<TState>, State<TState>>();
+        }
+        else
+        {
+            services.AddScoped(_ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()));
+            services.AddScoped<IStore<TState>, State<TState>>();
+        }
+        services.AddTransient<IState<TState>, State<TState>>(services => new State<TState>((State<TState>)services.GetRequiredService<IStore<TState>>()));
+        return services;
+    }
+
+}
