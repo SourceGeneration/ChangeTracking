64611d2eed3b7f37f2f12be90b34e44b5408e2cf
肖昕 <xiao.xin@outlook.com>
2024-02-17 14:21:25 +0100

    update sample

--- samples/SimpleState/Program.cs
+++ samples/SimpleState/Program.cs
@@ -2,11 +2,8 @@
 
 State<Goods> state = new(new Goods());
 
-var disposable = state.Bind(x => x.Price, x => Console.WriteLine($"Price changed: {x}"));
-state.Bind(x => x.Count, x => Console.WriteLine($"Count changed: {x}"));
-
-// no ouput, because the property of Number was not bound
-state.Update(x => x.Number = 1);
+var disposable = state.Bind(x => x.Price, x => Console.WriteLine($"Price has changed: {x}"));
+state.Bind(x => x.Count, x => Console.WriteLine($"Count has changed: {x}"));
 
 // ouput Price changed: 3.14
 // ouput Price changed: 3
@@ -16,16 +13,16 @@ state.Update(x =>
     x.Count = 3;
 });
 
-// no ouput, the value was not changed
+// no ouput, the value was has changed
 state.Update(x => x.Price = 3.14);
 
-// no ouput, the value was not changed
-state.Update(x => x.Count = 3);
+// no ouput, because the property of Number was not subscribe
+state.Update(x => x.Number = 1);
 
 // Unsubscribe price property binding 
 disposable.Dispose();
 
-// no ouput, the value was not changed
+// no ouput, the value was has changed
 state.Update(x => x.Price = 4.28);
 
 Console.ReadLine();
