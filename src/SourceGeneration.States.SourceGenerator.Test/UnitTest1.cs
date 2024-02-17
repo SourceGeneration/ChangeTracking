e2ebb03dd657a11cb15bd75d5282bd6032a4926a
肖昕 <xiao.xin@outlook.com>
2024-02-17 06:47:28 +0100

    update test

--- src/SourceGeneration.States.SourceGenerator.Test/UnitTest1.cs
+++ src/SourceGeneration.States.SourceGenerator.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace SourceGeneration.States.SourceGenerator.Test
 {
     [TestClass]
@@ -8,34 +10,15 @@ namespace SourceGeneration.States.SourceGenerator.Test
         {
             string source = @"
 using SourceGeneration.States;
+using Microsoft.Extensions.DependencyInjection;
 
-[ChangeTracking]
+[StateInject(ServiceLifetime.Transient)]
 public class TestModel
 {
     public required virtual int[] Array { get; set; } = [];
 }
 ";
-            CSharpTestGenerator.Generate<ChanageTrackingProxySourceGenerator>(source, typeof(ChangeTrackingAttribute).Assembly);
+            var result = CSharpTestGenerator.Generate<StateInjectSourceGenerator>(source, typeof(ChangeTrackingAttribute).Assembly, typeof(ServiceLifetime).Assembly);
         }
-
-//        [TestMethod]
-//        public void TestMethod2()
-//        {
-//            string source = @"
-//using SourceGeneration.States;
-
-//[ChangeTracking]
-//public partial class Obj1
-//{
-//    public string Text
-//    {
-//        get => GetText();
-//        set => SetText(value);
-//    }
-//}
-//";
-//            CSharpTestGenerator.Generate<ChanageTrackingPartialSourceGenerator>(source, typeof(ChangeTrackingAttribute).Assembly);
-//        }
-
     }
 }
