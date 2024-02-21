1f419208dddcaeb6da378e96004adfcc9e828942
肖昕 <xiao.xin@outlook.com>
2024-02-21 07:56:49 +0100

    Rename

---
+++ src/SourceGeneration.States/IScopedState.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SourceGeneration.States;
+
+public interface IScopedState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState> : IState, IObservable<TState>
+{
+    IScopedState<TState> CreateScope();
+    TState Value { get; }
+    IDisposable Bind<TValue>(Func<TState, TValue> selector, Action<TValue> subscriber, IEqualityComparer<TValue> comparer);
+    IDisposable Bind<TValue>(Func<TState, TValue> selector, Func<TValue, bool>? predicate, Action<TValue> subscriber, IEqualityComparer<TValue> equalityComparer);
+    IDisposable Bind<TValue>(Func<TState, TValue> selector, Action<TValue> subscriber, ChangeTrackingScope scope = ChangeTrackingScope.Root);
+    IDisposable Bind<TValue>(Func<TState, TValue> selector, Func<TValue, bool>? predicate, Action<TValue> subscriber, ChangeTrackingScope scope = ChangeTrackingScope.Root);
+    IDisposable SubscribeBindingChanged(Action<TState> next);
+}
