d8132387a6bb28974b21b71defedf0ab7acb9d3d
肖昕 <xiao.xin@outlook.com>
2024-05-05 19:49:32 +0800

    Add test method

---
+++ src/SourceGeneration.States.Test/InjectionTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SourceGeneration.States.Test;
+
+[TestClass]
+public class InjectionTest
+{
+    [TestMethod]
+    public void Test()
+    {
+        ServiceCollection services = new();
+        services.AddState<TrackingObject>();
+
+        var provider = services.BuildServiceProvider();
+
+        var a = provider.GetRequiredService<State<TrackingObject>>();
+        var b = provider.GetRequiredService<TrackingObject>();
+
+        Assert.AreEqual(a.Value, b);
+    }
+
+    
+}
