77d0ef46fe821f9f4666da3721a97df0dd36a387
肖昕 <xiao.xin@outlook.com>
2024-06-09 15:35:40 +0800

    Add DI method: state custom initialize

--- src/SourceGeneration.States/DependencyInjection/SourceGenerationStateServiceCollectionExtensions.cs
+++ src/SourceGeneration.States/DependencyInjection/SourceGenerationStateServiceCollectionExtensions.cs
@@ -15,21 +15,35 @@ public static class SourceGenerationStateServiceCollectionExtensions
         return services;
     }
 
-    public static IServiceCollection AddState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TState : class
+    public static IServiceCollection AddState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(
+        this IServiceCollection services,
+        TState state,
+        ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TState : class
     {
-        AddStateCore<TState>(services, serviceLifetime);
+        AddStateCore(services, state, serviceLifetime);
+        return services;
+    }
+
+    public static IServiceCollection AddState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(
+        this IServiceCollection services, 
+        ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TState : class
+    {
+        AddStateCore<TState>(services, null, serviceLifetime);
         return services;
     }
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static void AddState<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(ServiceLifetime serviceLifetime = ServiceLifetime.Scoped) where TState : class
     {
-        AddStateCore<TState>(_services, serviceLifetime);
+        AddStateCore<TState>(_services, null, serviceLifetime);
     }
 
-    private static void AddStateCore<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(IList<ServiceDescriptor> services, ServiceLifetime serviceLifetime) where TState : class
+    private static void AddStateCore<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor | DynamicallyAccessedMemberTypes.PublicConstructors)] TState>(
+        IList<ServiceDescriptor> services,
+        TState? state,
+        ServiceLifetime serviceLifetime) where TState : class
     {
-        services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(Activator.CreateInstance<TState>()), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(TState), _ => ChangeTrackingProxyFactory.Create(state ?? Activator.CreateInstance<TState>()), serviceLifetime));
         services.Add(new ServiceDescriptor(typeof(State<TState>), p => new State<TState>(p.GetRequiredService<TState>()), serviceLifetime));
         services.Add(new ServiceDescriptor(typeof(IScopedState<TState>), p => new State<TState>(p.GetRequiredService<State<TState>>()), ServiceLifetime.Transient));
     }
