9e937c9a964a9fa29f07c00b33da49eaa5cf7384
肖昕 <xiao.xin@outlook.com>
2024-08-12 14:43:14 +0800

    Create test

---
+++ src/SourceGeneration.ChangeTracking.Test/TrackObject/AbstractTypeTest.cs
@@ -0,0 +1,33 @@
+﻿namespace SourceGeneration.ChangeTracking;
+
+[TestClass]
+public class AbstractTypeTest
+{
+    [TestMethod]
+    public void Test()
+    {
+        DerivedType tracking = new();
+
+        ((ICascadingChangeTracking)tracking).AcceptChanges();
+
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+        tracking.A = 1;
+        Assert.IsTrue(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+    }
+}
+
+[ChangeTracking]
+public abstract partial class AbstractType
+{
+    public partial int A { get; set; }
+}
+
+[ChangeTracking]
+public partial class DerivedType :AbstractType
+{
+    public partial int B { get; set; }
+}
---
+++ src/SourceGeneration.ChangeTracking.Test/TrackObject/GenericTypeTest.cs
@@ -0,0 +1,27 @@
+﻿namespace SourceGeneration.ChangeTracking;
+
+[TestClass]
+public class GenericTypeTest
+{
+    [TestMethod]
+    public void Test()
+    {
+        GenericType<int> tracking = new();
+
+        ((ICascadingChangeTracking)tracking).AcceptChanges();
+
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+        tracking.A = 1;
+        Assert.IsTrue(((ICascadingChangeTracking)tracking).IsChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)tracking).IsCascadingChanged);
+
+    }
+}
+
+[ChangeTracking]
+public partial class GenericType<T>
+{
+    public partial int A { get; set; }
+}
