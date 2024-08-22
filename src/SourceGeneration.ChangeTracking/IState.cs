ceb758a4459c1544bb2de3ffa5f677940374b5d4
肖昕 <xiao.xin@outlook.com>
2024-08-22 08:50:47 +0800

    Remove IState interface

--- src/SourceGeneration.ChangeTracking/IState.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SourceGeneration.ChangeTracking;
-
-public interface IState<TSelf> where TSelf : class, IState<TSelf>
-{
-    IChangeTracker<TSelf> CreateTracker();
-    void AcceptChanges();
-}
