077952b85be5e9f24f0ea51689b89d8ed5eb31fc
肖昕 <xiao.xin@outlook.com>
2025-03-03 00:32:05 +0800

    add PropertyInitializerTest

---
+++ src/SourceGeneration.ChangeTracking.Test/State/PropertyInitializerTest.cs
@@ -0,0 +1,42 @@
+﻿namespace SourceGeneration.ChangeTracking.StateTest;
+
+[TestClass]
+public partial class PropertyInitializerTest
+{
+    [TestMethod]
+    public void InitializerTest()
+    {
+        DefaultValueObject o = new();
+
+        Assert.IsFalse(((ICascadingChangeTracking)o).IsBaseChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)o).IsCascadingChanged);
+
+        o.Value1.Value = 1;
+
+        Assert.IsFalse(((ICascadingChangeTracking)o).IsBaseChanged);
+        Assert.IsTrue(((ICascadingChangeTracking)o).IsCascadingChanged);
+
+        o.AcceptChanges();
+
+        Assert.IsFalse(((ICascadingChangeTracking)o).IsBaseChanged);
+        Assert.IsFalse(((ICascadingChangeTracking)o).IsCascadingChanged);
+
+    }
+
+
+    [ChangeTracking]
+    public partial class DefaultValueObject : State
+    {
+        public partial InnerObject Value1 { get; set; } = new();
+        public partial InnerObject Value2 { get; set; }
+    }
+
+    [ChangeTracking]
+    public partial class InnerObject
+    {
+        public partial int Value { get; set; }
+    }
+
+
+}
+
