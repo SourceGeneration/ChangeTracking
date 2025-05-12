3a04394b7a497eb67f35b97575b8c07c289b9751
肖昕 <xiao.xin@outlook.com>
2025-05-13 00:44:21 +0800

    add watch collection view support

--- src/SourceGeneration.ChangeTracking/IChangeTracker.cs
+++ src/SourceGeneration.ChangeTracking/IChangeTracker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace SourceGeneration.ChangeTracking;
 
@@ -14,5 +16,9 @@ public interface IChangeTracker<TState> : IChangeTracker where TState : class
     IDisposable Watch<TValue>(Func<TState, TValue> selector, Action<TValue>? subscriber, ChangeTrackingScope scope = ChangeTrackingScope.Root);
     IDisposable Watch<TValue>(Func<TState, TValue> selector, Func<TValue, bool>? predicate, ChangeTrackingScope scope = ChangeTrackingScope.Root);
     IDisposable Watch<TValue>(Func<TState, TValue> selector, Func<TValue, bool>? predicate, Action<TValue>? subscriber, ChangeTrackingScope scope = ChangeTrackingScope.Root);
+
+    IDisposable Watch<TItem>(Func<TState, ChangeTrackingList<TItem>> selector, Func<TItem, bool> predicate, Action<IEnumerable<TItem>>? subscriber = null, ChangeTrackingScope scope = ChangeTrackingScope.Root);
+    IDisposable Watch<TCollection, TItem>(Func<TState, TCollection> selector, Func<TItem, bool> predicate, Action<IEnumerable<TItem>>? subscriber = null, ChangeTrackingScope scope = ChangeTrackingScope.Root) where TCollection : IEnumerable<TItem>, INotifyCollectionChanged;
+
     IDisposable OnChange(Action<TState> subscriber);
 }
